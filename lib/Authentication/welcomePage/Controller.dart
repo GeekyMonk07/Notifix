c808be497ef172e7ad6c8401cf1b0fd71ce2830f
carbseater <madhavmishra28@gmail.com>
2021-11-29 00:36:31 +0530

    auth added

---
+++ lib/Authentication/welcomePage/Controller.dart
@@ -0,0 +1,39 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+// import 'package:glbapp/pages/HomeScreen/HomePage/homepage.dart';
+// import 'package:glbapp/pages/HomeScreen/homeScreen.dart';
+import 'package:appnewui/Authentication/welcomePage/welcome.dart';
+import 'package:appnewui/Authentication/welcomePage/body.dart';
+import 'package:appnewui/Authentication/adminlogin/adminlogin.dart';
+class Controller extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final user=FirebaseAuth.instance.currentUser;
+    // print(user);
+    return Scaffold(
+        body: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context,snapshot){
+            if(snapshot.connectionState==ConnectionState.waiting)
+              return Center(child: CircularProgressIndicator(),);
+            else if(snapshot.hasData) {
+              print("admin");
+              return AdminLogin();
+
+            }
+            else if(snapshot.hasError)
+              return Center(child: Text('something went wrong '),);
+            else{
+              print("Welcome page");
+
+              return WelcomePage();
+            }
+          },
+
+        )
+
+    );
+
+  }
+}
--- lib/Authentication/welcomePage/body.dart
+++ lib/Authentication/welcomePage/body.dart
@@ -2,11 +2,32 @@ import 'package:appnewui/Authentication/welcomePage/background.dart';
 import 'package:appnewui/Authentication/widget/button.dart';
 import 'package:appnewui/constrants.dart';
 import 'package:flutter/material.dart';
-
+import 'package:appnewui/Authentication/welcomePage/Controller.dart';
+import 'package:appnewui/Authentication/Auth/firebase.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:lottie/lottie.dart';
+import 'package:appnewui/Authentication/welcomePage/welcome.dart';
+import 'package:appnewui/Authentication/welcomePage/body.dart';
+import 'package:appnewui/Authentication/adminlogin/adminlogin.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
+  const Body({ Key? key }) : super(key: key);
+
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  void click() {
+    print("Signing with google clicked------>");
+    final provider=Provider.of<GoogleSignInProvider>(context,listen:false);
+    provider.signInWithGoogle();
+
+  }
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -29,32 +50,41 @@ class Body extends StatelessWidget {
                 style: TextStyle(fontWeight: FontWeight.normal, fontSize: 15),
               ),
               SizedBox(height: size.height * 0.05),
-              // Image.asset(
-              //   "assets/images/book.png",
-              //   height: size.height * .30,
-              // ),
+              
               Lottie.asset("assets/images/welcome.json", height: 250),
-              // SvgPicture.asset(
-              //   "assets/images/books.svg",
-              //   //fit: BoxFit.cover,
-              //   height: size.height * .30,
-              // ),
+              
               SizedBox(height: size.height * 0.05),
               CircularButton(
                 text: "LOGIN",
                 color: primaryColor,
                 textColor: Colors.white,
-                press: () => Navigator.pushNamed(context, "/login"),
+                press: () => this.click(),
               ),
-              // SizedBox(
-              //   height: 20,
-              // ),
-              // CircularButton(
-              //   text: "SIGN UP",
-              //   color: secondaryPurple,
-              //   textColor: Colors.black,
-              //   press: () => Navigator.pushNamed(context, "/adminlogin"),
-              // ),
+//               OutlinedButton(
+//                 onPressed: this.click,
+//                 style: OutlinedButton.styleFrom(
+//                   backgroundColor:  Color(0xff902525),
+// //splashColor: Colors.grey,
+//                   shadowColor: Colors.grey,
+//
+//                   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(45)),
+//
+// // BorderSide: BorderSide(color: Colors.grey),
+//
+//                 ),
+//                 child: Padding(
+//                     padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+//                     child: Row(
+//                       mainAxisSize: MainAxisSize.min,
+//                       mainAxisAlignment: MainAxisAlignment.center,
+//                       children: <Widget>[
+//                         // Image(image: AssetImage('assets/images/google_logo.png'), height: 35),
+//                         Padding(
+//                             padding: EdgeInsets.only(left: 10),
+//                             child: Text('Sign in with Google',
+//                                 style: TextStyle(color: Colors.white, fontSize: 25)))
+//                       ],
+//                     ))),
               SizedBox(height: size.height * 0.05),
             ],
           ),
@@ -63,3 +93,4 @@ class Body extends StatelessWidget {
     );
   }
 }
+
