a20f92b321faa52128afe05030225b1a8b045e72
carbseater <madhavmishra28@gmail.com>
2022-01-24 20:02:03 +0530

    new changes

--- lib/Authentication/welcomePage/Controller.dart
+++ lib/Authentication/welcomePage/Controller.dart
@@ -1,23 +1,23 @@
-// import 'package:appnewui/Authentication/adminlogin/adminlogin.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter/material.dart';
-// import 'package:appnewui/Authentication/welcomePage/welcome.dart';
-//
-// class Controller extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         body: StreamBuilder(
-//           stream: FirebaseAuth.instance.authStateChanges(),
-//           builder: (context, snapshot) {
-//             if (snapshot.connectionState == ConnectionState.waiting) {
-//               return Center(child: CircularProgressIndicator());
-//             }
-//             else if (snapshot.hasData) {
-//               return AdminLogin();
-//             }
-//             return WelcomePage();
-//           },
-//         ));
-//   }
-// }
+import 'package:appnewui/Authentication/adminlogin/adminlogin.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:appnewui/Authentication/welcomePage/welcome.dart';
+
+class Controller extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+            else if (snapshot.hasData) {
+              return AdminLogin();
+            }
+            return WelcomePage();
+          },
+        ));
+  }
+}
